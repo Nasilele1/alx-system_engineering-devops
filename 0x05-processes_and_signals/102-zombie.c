67d9d0b18810eb96ca201d20e9a1f1eef9829720
Nasilele1 <jasperkatondo@gmail.com>
2023-04-27 19:46:45 -0700

    102-zombie.c

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
+
+/**
+ * infinite_while - a function that runs for infinity and returns nothing
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * program that creates 5 zombie processes
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int children_process = 0;
+	pid_t pid;
+
+	while (children_process < 5)
+	{
+		pid = fork();
+		if (!pid)
+			break;
+		print("Zombie process created, PID: %i\n", (int)pid);
+		children_processes++;
+	}
+	if (pid != 0)
+		infinite_while();
+	return (0);
+}
